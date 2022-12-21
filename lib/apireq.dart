ad752dea1d8a4ce0fc49ca2987b5e521483a52de
BaiDom <dombaily88@gmail.com>
2022-12-21 14:40:36 +0000

    APOD displays on pageload

--- lib/apireq.dart
+++ lib/apireq.dart
@@ -5,12 +5,17 @@ import 'package:http/http.dart' as http;
 class APOD extends StatefulWidget {
   const APOD({super.key});
 
-  @override
   State<APOD> createState() => _APODState();
 }
 
 class _APODState extends State<APOD> {
   Map<String, dynamic>? starData;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchAPI();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +62,7 @@ class _APODState extends State<APOD> {
           Text(
               'Hey there Space Cadet, would you like to view an amazing astronomical picture from NASA everyday? You would! Well then click the button!'),
           ElevatedButton(
-            onPressed: fetchAPI,
+            onPressed: _fetchAPI,
             child: Text("Get Awesome Stuff"),
           )
         ],
@@ -79,7 +84,7 @@ class _APODState extends State<APOD> {
     );
   }
 
-  void fetchAPI() async {
+  _fetchAPI() async {
     const url =
         'https://api.nasa.gov/planetary/apod?api_key=iRDlURIVK0SQdDk3D5Wx1Rd1Tr9J4zP5YHSWWP91';
     var uri = Uri.parse(url);
